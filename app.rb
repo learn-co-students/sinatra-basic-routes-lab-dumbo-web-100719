9a3a25b2d019fa4c40272a0bf358af032b1a6ce9
Vannida Lim <vannida.lim@gmail.com>
2019-10-28 16:05:38 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Vannida"
+    end
+
+    get '/hometown' do
+        "My hometown is Philadelphia"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Can't Stop Me"
+    end
 end
