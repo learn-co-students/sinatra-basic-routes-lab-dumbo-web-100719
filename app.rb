1954fd41f512221e131948020e9a066c1e5e3688
Nabeel Siddiqui <nabeelsiddiqui@Nabeels-Air.fios-router.home>
2019-10-28 15:48:49 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Nabeel"
+    end
+
+    get '/hometown' do
+        "My hometown is Queens, NY"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Juicy"
+    end
+
 end
