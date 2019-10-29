ac423fd2b09a52ec88ad4910b00c5f682800ef8a
Javone <javosmith12@gmail.com>
2019-10-29 11:25:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Javone Smith"
+    end
+
+    get '/hometown' do
+        "My hometown is Uniondale"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is My Way by Frank Sinatra"
+    end
+
 end
