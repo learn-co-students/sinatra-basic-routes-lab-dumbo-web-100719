795fcb96d709c17bc40628f1373f93684b900391
Jon Caneiro <joncaneiro@gmail.com>
2019-10-29 11:45:26 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/name" do
+        "My name is Jon"
+    end
+
+    get "/hometown" do
+        "My hometown is Brooklyn"
+    end
+
+    get "/favorite-song" do
+        "My favorite song is a good one"
+    end
+
 end
